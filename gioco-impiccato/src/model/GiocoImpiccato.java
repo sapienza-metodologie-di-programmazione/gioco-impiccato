2033d54ec6918f55d4df2c4afd89f9d95fd4a55c
Arianna Paolini <ariannapaolini011@gmail.com>
2024-08-12 11:03:10 +0200

    implemented model

--- gioco-impiccato/src/model/GiocoImpiccato.java
+++ gioco-impiccato/src/model/GiocoImpiccato.java
@@ -1,13 +1,13 @@
 package model;
 
 import java.nio.file.Path;
+import java.util.Observable;
 import java.util.Optional;
 import java.util.Set;
 
-public class GiocoImpiccato {
+public class GiocoImpiccato extends Observable {
 
-	private static Set<String> parole;
-
+	private Set<String> parole;
 	private int partiteGiocate;
 	private int partiteVinte;
 	private Optional<String> ultimaParolaIndovinata;
@@ -28,4 +28,48 @@ public class GiocoImpiccato {
 		return partiteVinte;
 	}
 
+	public Optional<String> getUltimaParolaIndovinata() {
+		return ultimaParolaIndovinata;
+	}
+
+	public Optional<PartitaImpiccato> getPartitaCorrente() {
+		return partitaCorrente;
+	}
+
+	public void aumentaPartiteGiocate() {
+		partiteGiocate++;
+	}
+
+	public void aumentaPartiteVinte() {
+		partiteVinte++;
+	}
+
+	public void setUltimaParolaIndovinata(String parola) {
+		ultimaParolaIndovinata = Optional.of(parola);
+	}
+
+	/**
+	 * Metodo per creare una nuova partita del Gioco dell'Impiccato; se un'altra
+	 * partita era già in corso essa viene sovrascritta
+	 * 
+	 * @return true se la nuova partita non sovrascrive alcuna partita
+	 *         pre-esistente, false altrimenti
+	 */
+	public boolean iniziaPartita() {
+		boolean b = partitaCorrente.isEmpty();
+		partitaCorrente = Optional.of(new PartitaImpiccato(this, "boh"));
+		return b;
+	}
+
+	/**
+	 * Metodo per terminare la partita corrente del Gioco dell'Impiccato
+	 * 
+	 * @return true se è presente una partita da terminare, false altrimentis
+	 */
+	public boolean terminaPartita() {
+		boolean b = partitaCorrente.isPresent();
+		if (b)
+			partitaCorrente = Optional.empty();
+		return b;
+	}
 }
--- gioco-impiccato/src/model/PartitaImpiccato.java
+++ gioco-impiccato/src/model/PartitaImpiccato.java
@@ -1,5 +1,72 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class PartitaImpiccato {
 
+	public enum StatoPartita {
+		IN_CORSO, PERSA, VINTA
+	}
+
+	private GiocoImpiccato gioco;
+	private StatoPartita stato;
+	private String parola; // parola da indovinare
+	private int errori; // conteggio errori commessi con le lettere scelte
+	private Set<Character> lettereParola; // lettere che compongono la parola da indovinare
+	private Set<Character> lettereUsate; // lettere selezionate dal giocatore fino al momento corrente
+
+	public static final int MAX_ERRORI = 5; // massimo numero di errori concessi prima di perdere la partita
+
+	public PartitaImpiccato(GiocoImpiccato gioco, String parola) {
+		this.gioco = gioco;
+		stato = StatoPartita.IN_CORSO;
+		this.parola = parola;
+		errori = 0;
+		lettereParola = parola.chars().mapToObj(c -> (Character) (char) c).collect(Collectors.toSet());
+		lettereUsate = new HashSet<Character>();
+	}
+
+	public GiocoImpiccato getGioco() {
+		return gioco;
+	}
+
+	public StatoPartita getStato() {
+		return stato;
+	}
+
+	public String getParola() {
+		return parola;
+	}
+
+	public int getErrori() {
+		return errori;
+	}
+
+	public Set<Character> getLettereParola() {
+		return lettereParola;
+	}
+
+	public Set<Character> getLettereUsate() {
+		return lettereUsate;
+	}
+
+	public void aumentaErrori() {
+		errori++;
+	}
+
+	public void aggiungiLetteraUsata(Character lettera) {
+		lettereUsate.add(lettera);
+	}
+
+	// metodo per controllare se si sono verificate le condizioni di vittoria
+	// della partita
+	public boolean checkVittoria() {
+		return !checkSconfitta() && lettereUsate.containsAll(lettereParola);
+	}
+
+	public boolean checkSconfitta() {
+		return errori > MAX_ERRORI;
+	}
 }
